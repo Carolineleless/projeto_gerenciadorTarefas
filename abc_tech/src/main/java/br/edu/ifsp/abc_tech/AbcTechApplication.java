0cbf39602de4f6292bb7dbdb8b187037ce1cb6ca
pedro-rampazo <pedro.rampazo@hotmail.com>
2023-08-30 12:21:47 -0300

    criando projeto spring boot

---
+++ abc_tech/src/main/java/br/edu/ifsp/abc_tech/AbcTechApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.abc_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbcTechApplication.class, args);
+    }
+
+}
---
+++ abc_tech/src/test/java/br/edu/ifsp/abc_tech/AbcTechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.abc_tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
